02b21f3d49d415b2a4ea33bdca6bc4372df8a753
ap-lodhi <anilpatel.bcs@gmail.com>
2023-04-23 11:06:02 +0530

    DONE

---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const projects = [
+  {
+    title: "E-commerce App",
+    subtitle: "TECH STACK: HTML | CSS | JavaScript | React.js | Local Storage",
+    description: " An Online Store Where User Can By Products. Website Was Built With All Features.",
+    responsibility:
+      " Developed product page,add to cart page and add search and short functions and filters. Sort functions for Higher to Lower price,Lower to Higher price and Search bar for finding the product by it's name, Add to cart.",
+    team: "An individual project - developed within 20 days.",
+    image: "https://user-images.githubusercontent.com/87072168/215344512-89cc2a84-90e0-4acf-925f-d6c0b110a48b.JPG",
+    gitlink: "https://github.com/ap-lodhi/e-commerce",
+    link: "https://clinquant-unicorn-c44fb9.netlify.app/",
+  },
+  {
+    title: "CoSchedule Clone",
+    subtitle: "TECH STACK: HTML | CSS | JavaScript | JSON Server | Local Storage",
+    description:
+      "CoSchedule is an all-in-one platform for planning, organizing, and executing all things related to content marketing.", 
+      responsibility:
+      "Developed CoSchedule Guide Page, That show how to create CoSchedule Marketing Calendar.",
+    team: " A cooperative Project worked by a group of 5 executed in 6 days.",
+    image: "/coshedule.png",
+    gitlink: "https://github.com/ap-lodhi/coSchedule-clone",
+    link: "ap-lodhi.github.io/coSchedule-clone/",
+  },
+  {
+    title: " MyFitnessPal",
+    subtitle:
+      "TECH STACK: HTML | CSS | JavaScript | JSON Server | LocalStorage",
+    description:
+      "  MyFitnessPal is a smartphone app and website that tracks diet and exercise website was built with all features.",
+    responsibility:
+      " Developed Profile Page and responsive footer Blog Page,Community Post page.",
+    team: " A cooperative Project worked by a group of 4 executed in 6 days.",
+    image: "./myFitnesspal.png",
+    gitlink: "https://github.com/Ankit-Ankushe/myFitnessPal-Clone",
+    link: "https://lifestylepalteam.netlify.app/",
+  },
+  {
+    title: "Travelocity.com Clone",
+    subtitle:
+      "TECH STACK: ReactJS | Redux | JavaScript | JSON Server | Google Auth | Material-UI",
+    description:
+      "  A simple app which have only two button . on one click we can add photo and we can remove photo . we can add unlimited photos. Developed the entire Application along with all Functionality.",
+    responsibility:
+      " An individual project - developed within 1 days.",
+    team: " .",
+    image: "https://user-images.githubusercontent.com/87072168/198592900-c1eb6644-551b-43cf-bb70-d33d89b265bf.JPG",
+    gitlink: "https://github.com/ap-lodhi/dynamic-photo",
+    link: "https://internship-cyan.vercel.app/",
+  }
+  
+];
