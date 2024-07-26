cbfacf21b3be1841e0d7cb13d71259bf7c11eb19
ap-lodhi <anilpatel.bcs@gmail.com>
2024-07-26 12:43:17 +0530

    project updated

--- src/data.js
+++ src/data.js
@@ -1,21 +1,48 @@
 export const projects = [
   {
+    title: "Ambit CRM View",
+    subtitle: "TECH STACK: ASP.NET Core | ASP.NET MVC | MS SQL Server",
+    description:
+      "Developed a comprehensive CRM solution for Ambit, a leading provider of financial advice and capital solutions, to enhance data filtering and user experience.",
+    responsibility:
+      "Implemented robust search functionalities, including universal and advanced searches (exact, similar, related), added bookmarking for table rows, displayed previous search history, and enabled secure login using organization email.",
+    team: " A cooperative Project worked by a group of 3",
+    image: "/ambit.jpg",
+    gitlink: "https://github.com/ap-lodhi/Ambit_CRM",
+    link: "https://crmview.ambit.co/",
+  },
+  {
+    title: "Cylsys Admin Panel",
+    subtitle: "TECH STACK: ASP.NET MVC | jQuery | MySQL",
+    description:
+      "Web application like managing and organising content, tracking website analytics, managing user accounts, and performing various other tasks.",
+    responsibility:
+      "Implemented server-side logic and database interactions using ASP.NET MVC and MySQL.Designed and maintained the database schema to support the application's data requirements.",
+    team: " A cooperative Project worked by a group of 5",
+    image: "/cylsys admin.jpg",
+    gitlink: "https://github.com/ap-lodhi/CylsysAdminPanel",
+    link: "https://admin.cylsys.com/",
+  },
+  {
     title: "E-commerce App",
     subtitle: "TECH STACK: HTML | CSS | JavaScript | React.js | Local Storage",
-    description: " An Online Store Where User Can By Products. Website Was Built With All Features.",
+    description:
+      " An Online Store Where User Can By Products. Website Was Built With All Features.",
     responsibility:
       " Developed product page,add to cart page and add search and short functions and filters. Sort functions for Higher to Lower price,Lower to Higher price and Search bar for finding the product by it's name, Add to cart.",
     team: "An individual project - developed within 20 days.",
-    image: "https://user-images.githubusercontent.com/87072168/215344512-89cc2a84-90e0-4acf-925f-d6c0b110a48b.JPG",
+    image:
+      "https://user-images.githubusercontent.com/87072168/215344512-89cc2a84-90e0-4acf-925f-d6c0b110a48b.JPG",
     gitlink: "https://github.com/ap-lodhi/e-commerce",
     link: "https://clinquant-unicorn-c44fb9.netlify.app/",
   },
   {
     title: "CoSchedule Clone",
-    subtitle: "TECH STACK: HTML | CSS | JavaScript | JSON Server | Local Storage",
+    subtitle:
+      "TECH STACK: HTML | CSS | JavaScript | JSON Server | Local Storage",
     description:
-      "CoSchedule is an all-in-one platform for planning, organizing, and executing all things related to content marketing.", 
-      responsibility:
+      "CoSchedule is an all-in-one platform for planning, organizing, and executing all things related to content marketing.",
+    responsibility:
       "Developed CoSchedule Guide Page, That show how to create CoSchedule Marketing Calendar.",
     team: " A cooperative Project worked by a group of 5 executed in 6 days.",
     image: "/coshedule.png",
@@ -34,19 +61,17 @@ export const projects = [
     image: "./myFitnesspal.png",
     gitlink: "https://github.com/Ankit-Ankushe/myFitnessPal-Clone",
     link: "https://lifestylepalteam.netlify.app/",
-  },
-  {
-    title: "Dynamic Photo App",
-    subtitle:
-      "TECH STACK: ReactJS | Redux | JavaScript | JSON Server ",
-    description:
-      "  A simple app which have only two button . on one click we can add photo and we can remove photo . we can add unlimited photos. Developed the entire Application along with all Functionality.",
-    responsibility:
-      " An individual project - developed within 1 days.",
-    team: " .",
-    image: "https://user-images.githubusercontent.com/87072168/198592900-c1eb6644-551b-43cf-bb70-d33d89b265bf.JPG",
-    gitlink: "https://github.com/ap-lodhi/dynamic-photo",
-    link: "https://internship-cyan.vercel.app/",
   }
-  
+  // {
+  //   title: "Dynamic Photo App",
+  //   subtitle: "TECH STACK: ReactJS | Redux | JavaScript | JSON Server ",
+  //   description:
+  //     "  A simple app which have only two button . on one click we can add photo and we can remove photo . we can add unlimited photos. Developed the entire Application along with all Functionality.",
+  //   responsibility: " An individual project - developed within 1 days.",
+  //   team: " .",
+  //   image:
+  //     "https://user-images.githubusercontent.com/87072168/198592900-c1eb6644-551b-43cf-bb70-d33d89b265bf.JPG",
+  //   gitlink: "https://github.com/ap-lodhi/dynamic-photo",
+  //   link: "https://internship-cyan.vercel.app/",
+  // },
 ];
